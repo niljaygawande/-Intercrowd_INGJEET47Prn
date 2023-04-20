45deec1a519ae4e42705fc992da8755ff831a62b
niljaygawande <131400739+niljaygawande@users.noreply.github.com>
2023-04-21 01:11:47 +0530

    Add files via upload

---
+++ Phonebook_JAVA/Phonebookjava/backup.sql
@@ -0,0 +1,3 @@
+insert into notebook.table2 values ('karan' , 'pune' , '1234567890');
+insert into notebook.table2 values ('niljay' , 'chandrapur' , '7447571312');
+insert into notebook.table2 values ('ajay' , 'jalna' , '9011913776');
